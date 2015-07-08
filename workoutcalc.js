ec0c7c42005d94becb5c0de33b80ae95c07b41b3
Alesh Houdek <mail@alesh.com>
2015-07-08 08:05:19 -0400

    Create readme file

--- workoutcalc.js
+++ workoutcalc.js
@@ -1,33 +1,3 @@
-
-// suggest weights combination
-// use keyboard to control weight input
-// fix weight input buttons
-// convert all input divs to buttons
-// utility function to delete local data
-//
-// 2015-07-08 separated out js css html files
-// 2015-06-11 stored value bugfix, status bar changes
-// 2015-06-09 fixed inc button from 0
-// 2015-05-27 properly encode and retrieve dates in localStorage
-// 2015-05-26 display previous weight on weight entry screen
-// 2015-05-24 next time weight input view flow)
-// 2015-05-23 de-lint :), started in on next-time weight view
-// 2015-05-22 next exercise inc by +10 or less to round up, fixes
-// 2015-05-21 smart +/- buttons on weight input screen
-// 2015-05-20 store/retrive past workouts, get value for each exercise, pre-populate field
-// 2015-05-16 finally figure out workout storage object, display results after workout
-// 2015-05-12 fix view nesting, show stored values at end
-// 2015-05-11 iphone refinements, stack exchange submit
-// 2015-05-10 responsive design
-// 2015-05-09 improved MVC flow and functions, step through three exercises
-// 2015-05-08 css styling, minimum weight, testing mode to skip first screen
-// 2015-05-07 css styling, weight input
-// 2015-05-06 display sets
-// 2015-05-05 more logic
-// 2015-05-04 event handling for workout selection
-// 2015-05-03 calculate sets and weights
-// 2015-05-02 started -- basic HTML and CSS
-
 var model = {
   exercises: ['sq', 'bp', 'dl', 'sp', 'pc'],
   currentExerciseWeights: [],
